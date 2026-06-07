096686f91cf41a964174171087c7818f3f5e9c24
lliikkii7722-ops <lliikkii7722@gmail.com>
2026-06-07 18:30:23 +0530

    Connected frontend to deployed backend

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2008/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL: "https://devloop-backend.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
